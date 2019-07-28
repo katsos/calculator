457bdfd4f0644fd6984ffcfc1a78f024e7b43da0
Nikos Katsos <nikoskatsos101@gmail.com>
2019-07-28 20:58:33 +0300

    implemented basic functionality
    using `eval` and an array of factors(strings) to keep the state of the expression

--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import last from 'lodash/last';
 import ButtonPanel from './components/ButtonPanel';
-import './App.css';
+import './App.scss';
 
-function App() {
-  function onClick(e) {
-    console.log(e);
+const INITIAL_STATE = {
+  expressionFactors: [],
+  result: null,
+};
+const SEPARATORS = ['÷', '*', '-', '+'];
+
+class App extends PureComponent {
+  state = {...INITIAL_STATE}
+
+  onClick = (char) => {
+    switch (char) {
+      case 'C':
+        return this.setState(INITIAL_STATE);
+      case '=':
+        return this.setState({ expressionFactors: [this.state.result], result: null });
+      default:
+        this.updateExpression(char, this.updateResult);
+    }
   }
 
-  return (
-    <div className='App'>
-      <ButtonPanel onClick={onClick}/>
-    </div>
-  );
+  updateExpression(char, cb) {
+    const { expressionFactors } = this.state;
+    const isNewCharSeparator = SEPARATORS.includes(char);
+    const lastFactor = last(expressionFactors);
+    const isLastFactorSeparator = SEPARATORS.includes(lastFactor);
+
+    const newExpressionFactors = isNewCharSeparator ?
+      isLastFactorSeparator ? [...expressionFactors.slice(0, -1), char] // replace last separator
+      : [...expressionFactors, char] // add new separator
+      : [...expressionFactors.slice(0, -1), (lastFactor || '') + char]; // add or update number
+    this.setState({ expressionFactors: newExpressionFactors }, cb);
+  }
+
+  updateResult() {
+    const { expressionFactors: expression } = this.state;
+    const isLastFactorNumber = last(expression).match(/\d+/);
+    if (!isLastFactorNumber) return;
+    this.setState({ result: eval(expression.join('')) });
+  }
+
+  render() {
+    const { expressionFactors: expression, result } = this.state;
+
+    return (
+      <div className='App'>
+        <div className='App__display'>
+          <p className='App__display__expression'>{expression}</p>
+          <p className='App__display__result'>{result}</p>
+        </div>
+        <ButtonPanel onClick={this.onClick}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -11,26 +11,20 @@ const BUTTONS = {
     '1', '2', '3',
   ],
   BOTTOM: ['+/-', '0', '.'],
-  SIDE: ['÷', 'X', '-', '+', '='],
+  SIDE: ['÷', '*', '-', '+', '='],
 };
+const { TOP, NUMBERS, BOTTOM, SIDE } = BUTTONS;
 
-const ButtonPanel = (props) => {
-  function onClick(name) {
-    props.onClick(name);
-  }
-
-  const { TOP, NUMBERS, BOTTOM, SIDE } = BUTTONS;
-  return (
-    <div className='ButtonPanel'>
-      <div className='ButtonPanel__left'>
-        {[...TOP, ...NUMBERS, ...BOTTOM].map(b => <Button key={b} name={b} onClick={onClick} />)}
-      </div>
-      <div className='ButtonPanel__right'>
-        {SIDE.map(b => <Button key={b} name={b} onClick={onClick} />)}
-      </div>
+const ButtonPanel = ({ onClick }) => (
+  <div className='ButtonPanel'>
+    <div className='ButtonPanel__left'>
+      {[...TOP, ...NUMBERS, ...BOTTOM].map(b => <Button key={b} name={b} onClick={() => onClick(b)} />)}
     </div>
-  );
-};
+    <div className='ButtonPanel__right'>
+      {SIDE.map(b => <Button key={b} name={b} onClick={() => onClick(b)} />)}
+    </div>
+  </div>
+);
 
 ButtonPanel.propTypes = {
   onClick: PropTypes.func.isRequired,
