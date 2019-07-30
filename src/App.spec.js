c7209fdff72d2d7778f4cc0bf3e536b14f710b08
Nikos Katsos <nikoskatsos101@gmail.com>
2019-07-30 11:41:32 +0300

    removed redundant describe

--- src/App.spec.js
+++ src/App.spec.js
@@ -9,46 +9,44 @@ describe('App', () => {
       wrapper = shallow(<App />);
     });
 
-    describe('should update expression factors', () => {
-      beforeEach(() => {
-        wrapper.state().expressionFactors = [];
-      });
+    beforeEach(() => {
+      wrapper.state().expressionFactors = [];
+    });
 
-      it('should add the new number', () => {
-        wrapper.instance().handleInput('5');
-        const expressionFactors = wrapper.state().expressionFactors;
-        expect(expressionFactors).toEqual(['5']);
-      });
+    it('should add the new number', () => {
+      wrapper.instance().handleInput('5');
+      const expressionFactors = wrapper.state().expressionFactors;
+      expect(expressionFactors).toEqual(['5']);
+    });
 
-      it ('should update the last number', () => {
-        wrapper.instance().handleInput('5');
-        wrapper.instance().handleInput('5');
-        const expressionFactors = wrapper.state().expressionFactors;
-        expect(expressionFactors).toEqual(['55']);
-      });
+    it('should update the last number', () => {
+      wrapper.instance().handleInput('5');
+      wrapper.instance().handleInput('5');
+      const expressionFactors = wrapper.state().expressionFactors;
+      expect(expressionFactors).toEqual(['55']);
+    });
 
-      it ('should add the new separator', () => {
-        wrapper.instance().handleInput('5');
-        wrapper.instance().handleInput('+');
-        const expressionFactors = wrapper.state().expressionFactors;
-        expect(expressionFactors).toEqual(['5', '+']);
-      });
+    it('should add the new separator', () => {
+      wrapper.instance().handleInput('5');
+      wrapper.instance().handleInput('+');
+      const expressionFactors = wrapper.state().expressionFactors;
+      expect(expressionFactors).toEqual(['5', '+']);
+    });
 
-      it ('should replace last separator', () => {
-        wrapper.instance().handleInput('5');
-        wrapper.instance().handleInput('+');
-        wrapper.instance().handleInput('-');
-        const expressionFactors = wrapper.state().expressionFactors;
-        expect(expressionFactors).toEqual(['5', '-']);
-      });
+    it('should replace last separator', () => {
+      wrapper.instance().handleInput('5');
+      wrapper.instance().handleInput('+');
+      wrapper.instance().handleInput('-');
+      const expressionFactors = wrapper.state().expressionFactors;
+      expect(expressionFactors).toEqual(['5', '-']);
+    });
 
-      it ('should add the new number after last separator', () => {
-        wrapper.instance().handleInput('5');
-        wrapper.instance().handleInput('-');
-        wrapper.instance().handleInput('2');
-        const expressionFactors = wrapper.state().expressionFactors;
-        expect(expressionFactors).toEqual(['5', '-', '2']);
-      });
+    it('should add the new number after last separator', () => {
+      wrapper.instance().handleInput('5');
+      wrapper.instance().handleInput('-');
+      wrapper.instance().handleInput('2');
+      const expressionFactors = wrapper.state().expressionFactors;
+      expect(expressionFactors).toEqual(['5', '-', '2']);
     });
   });
 });
