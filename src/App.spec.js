195ae861576508b49df402857b93dca24f419cd9
Nikos Katsos <nikoskatsos101@gmail.com>
2019-07-30 11:39:43 +0300

    updated function name

--- src/App.spec.js
+++ src/App.spec.js
@@ -3,7 +3,7 @@ import App from './App';
 import { shallow } from 'enzyme';
 
 describe('App', () => {
-  describe('onClick', () => {
+  describe('handleInput', () => {
     let wrapper;
     beforeAll(() => {
       wrapper = shallow(<App />);
@@ -15,37 +15,37 @@ describe('App', () => {
       });
 
       it('should add the new number', () => {
-        wrapper.instance().onClick('5');
+        wrapper.instance().handleInput('5');
         const expressionFactors = wrapper.state().expressionFactors;
         expect(expressionFactors).toEqual(['5']);
       });
 
       it ('should update the last number', () => {
-        wrapper.instance().onClick('5');
-        wrapper.instance().onClick('5');
+        wrapper.instance().handleInput('5');
+        wrapper.instance().handleInput('5');
         const expressionFactors = wrapper.state().expressionFactors;
         expect(expressionFactors).toEqual(['55']);
       });
 
       it ('should add the new separator', () => {
-        wrapper.instance().onClick('5');
-        wrapper.instance().onClick('+');
+        wrapper.instance().handleInput('5');
+        wrapper.instance().handleInput('+');
         const expressionFactors = wrapper.state().expressionFactors;
         expect(expressionFactors).toEqual(['5', '+']);
       });
 
       it ('should replace last separator', () => {
-        wrapper.instance().onClick('5');
-        wrapper.instance().onClick('+');
-        wrapper.instance().onClick('-');
+        wrapper.instance().handleInput('5');
+        wrapper.instance().handleInput('+');
+        wrapper.instance().handleInput('-');
         const expressionFactors = wrapper.state().expressionFactors;
         expect(expressionFactors).toEqual(['5', '-']);
       });
 
       it ('should add the new number after last separator', () => {
-        wrapper.instance().onClick('5');
-        wrapper.instance().onClick('-');
-        wrapper.instance().onClick('2');
+        wrapper.instance().handleInput('5');
+        wrapper.instance().handleInput('-');
+        wrapper.instance().handleInput('2');
         const expressionFactors = wrapper.state().expressionFactors;
         expect(expressionFactors).toEqual(['5', '-', '2']);
       });
