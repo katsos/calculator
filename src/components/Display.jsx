508a36c65f03ad6fc14aeb52a89f63ed943bdfb4
Nikos Katsos <nikoskatsos101@gmail.com>
2019-07-31 22:58:43 +0300

    format numbers in expression
    enclose negative number in a (-x) format
    show big numbers in comma separated format

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { numberWithCommas } from '../helpers';
 import './Display.scss';
 
 class Display extends React.PureComponent {
@@ -10,15 +11,21 @@ class Display extends React.PureComponent {
     this.expressionRef.current.scroll(0, scrollHeight - offsetHeight);
   }
 
-  render() {
-    const { result, expressionFactors } = this.props;
-    return (
-      <div className='Display'>
-        <p className='Display__result'>{result}</p>
-        <p className='Display__expression' ref={this.expressionRef}>{expressionFactors.join(' ')}</p>
-      </div>
-    )
+  expressionFactorsDisplayFormat() {
+    return this.props.expressionFactors.map((v) => {
+      const number = Number(v);
+      if (!number || v === '0.') return v;
+      const numberCommas = numberWithCommas(v);
+      return (number < 0) ? `(-${Math.abs(numberCommas)})` : numberCommas;
+    }).join(' ');
   }
+
+  render = () => (
+    <div className='Display'>
+      <p className='Display__result'>{this.props.result}</p>
+      <p className='Display__expression' ref={this.expressionRef}>{this.expressionFactorsDisplayFormat()}</p>
+    </div>
+  )
 }
 
 export default Display;
--- src/helpers.js
+++ src/helpers.js
@@ -5,3 +5,9 @@ export function getLast(array) {
 export function getAllButLast(array) {
   return array.slice(0, -1);
 }
+
+export function numberWithCommas(number) {
+  const [intPart, decPart] = number.toString().split('.');
+  const integerWithCommas = intPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+  return integerWithCommas + (decPart ? `.${decPart}` : '');
+}
